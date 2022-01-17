e9c26d9026524343941fe7d6d398df66561a0b28
claudgomezg <claudgomez@gmail.com>
2022-01-17 12:20:45 -0300

    push de prueba

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Vota 1, Vota Brownie
         </p>
         <a
           className="App-link"
